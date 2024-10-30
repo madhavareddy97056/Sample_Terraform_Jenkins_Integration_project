8db18f095b07d674964d37e68cfc06a4cd44e3d3
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-10-30 18:48:58 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "jenkins-terrafrm-integration" {
+  ami                    = "ami-0dee22c13ea7a9a67"
+  availability_zone      = "ap-south-1a"
+  instance_type          = "t2.micro"
+  key_name               = "linuxkey"
+  vpc_security_group_ids = ["sg-02cd4172d34747bc7"]
+  tags = {
+    Name = "Jenkins_server"
+  }
+}
