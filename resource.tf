73ef455ac3a1cceeef9a99f9774fec673d943899
madhavareddy97056 <81456216+madhavareddy97056@users.noreply.github.com>
2024-10-30 19:46:47 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "jenkins-terrafrm-integration" {
-  ami                    = "ami-0dee22c13ea7a9a67"
+  ami_id                    = "ami-0dee22c13ea7a9a67"
   availability_zone      = "ap-south-1a"
   instance_type          = "t2.micro"
   key_name               = "linuxkey"
